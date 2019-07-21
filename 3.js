094984ad7501340dc0c5758b0e1124542ab0a0a6
1584269114 <53136163+1584269114@users.noreply.github.com>
2019-07-21 21:12:18 +0800

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,11 @@
+
+document.getElementById("button").onclick=点击;
+
+function 点击(){
+    var 账户=document.getElementById("account");
+    var 密码=document.getElementById("password");
+    var 邮箱=document.getElementById("Emale");
+    var 电话=document.getElementById("plone");
+    var 识别码=document.getElementById("code");
+    
+} 
